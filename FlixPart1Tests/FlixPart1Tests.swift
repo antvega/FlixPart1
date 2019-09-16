3dca739b8017b84affbc6e75846683e884061df5
Antonio Vega <antoniovegajr@Antonios-MBP.attlocal.net>
2019-09-16 13:55:17 -0700

    first commit

---
+++ FlixPart1Tests/FlixPart1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FlixPart1Tests.swift
+//  FlixPart1Tests
+//
+//  Created by Antonio Vega Jr on 9/9/19.
+//  Copyright © 2019 Antonio Vega Jr. All rights reserved.
+//
+
+import XCTest
+@testable import FlixPart1
+
+class FlixPart1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
